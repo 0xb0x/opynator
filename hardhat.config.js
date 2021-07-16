a7a77f4dffc3dfe9feb0adb586400e65e2bdc1e1
uselessbox <abrib00x@protonmail.com>
2021-07-16 11:57:57 +0100

    fix compiler issues

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,35 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.3",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: "0.6.2",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: "0.6.10",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      }
+    ],
+  },
 };
