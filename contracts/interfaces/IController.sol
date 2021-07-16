c336e6e0fd6b1d4bd9eb56b580a2d67a9716f464
uselessbox <abrib00x@protonmail.com>
2021-07-16 11:59:07 +0100

    opyn controller interface

--- contracts/interfaces/IController.sol
+++ contracts/interfaces/IController.sol
@@ -2,118 +2,16 @@
  * SPDX-License-Identifier: UNLICENSED
  */
 pragma solidity =0.6.10;
+pragma experimental ABIEncoderV2;
 
 import {MarginVault} from "../libs/MarginVault.sol";
 import {Actions} from "../libs/Actions.sol";
 interface Controller {
-    
-    /// @notice emits an event when an account operator is updated for a specific account owner
-    event AccountOperatorUpdated(address indexed accountOwner, address indexed operator, bool isSet);
-    /// @notice emits an event when a new vault is opened
-    event VaultOpened(address indexed accountOwner, uint256 vaultId, uint256 indexed vaultType);
-    /// @notice emits an event when a long oToken is deposited into a vault
-    event LongOtokenDeposited(
-        address indexed otoken,
-        address indexed accountOwner,
-        address indexed from,
-        uint256 vaultId,
-        uint256 amount
-    );
-    /// @notice emits an event when a long oToken is withdrawn from a vault
-    event LongOtokenWithdrawed(
-        address indexed otoken,
-        address indexed AccountOwner,
-        address indexed to,
-        uint256 vaultId,
-        uint256 amount
-    );
-    /// @notice emits an event when a collateral asset is deposited into a vault
-    event CollateralAssetDeposited(
-        address indexed asset,
-        address indexed accountOwner,
-        address indexed from,
-        uint256 vaultId,
-        uint256 amount
-    );
-    /// @notice emits an event when a collateral asset is withdrawn from a vault
-    event CollateralAssetWithdrawed(
-        address indexed asset,
-        address indexed AccountOwner,
-        address indexed to,
-        uint256 vaultId,
-        uint256 amount
-    );
-    /// @notice emits an event when a short oToken is minted from a vault
-    event ShortOtokenMinted(
-        address indexed otoken,
-        address indexed AccountOwner,
-        address indexed to,
-        uint256 vaultId,
-        uint256 amount
-    );
-    /// @notice emits an event when a short oToken is burned
-    event ShortOtokenBurned(
-        address indexed otoken,
-        address indexed AccountOwner,
-        address indexed from,
-        uint256 vaultId,
-        uint256 amount
-    );
-    /// @notice emits an event when an oToken is redeemed
-    event Redeem(
-        address indexed otoken,
-        address indexed redeemer,
-        address indexed receiver,
-        address collateralAsset,
-        uint256 otokenBurned,
-        uint256 payout
-    );
-    /// @notice emits an event when a vault is settled
-    event VaultSettled(
-        address indexed accountOwner,
-        address indexed oTokenAddress,
-        address to,
-        uint256 payout,
-        uint256 vaultId,
-        uint256 indexed vaultType
-    );
-    /// @notice emits an event when a vault is liquidated
-    event VaultLiquidated(
-        address indexed liquidator,
-        address indexed receiver,
-        address indexed vaultOwner,
-        uint256 auctionPrice,
-        uint256 auctionStartingRound,
-        uint256 collateralPayout,
-        uint256 debtAmount,
-        uint256 vaultId
-    );
-    /// @notice emits an event when a call action is executed
-    event CallExecuted(address indexed from, address indexed to, bytes data);
-    /// @notice emits an event when the fullPauser address changes
-    event FullPauserUpdated(address indexed oldFullPauser, address indexed newFullPauser);
-    /// @notice emits an event when the partialPauser address changes
-    event PartialPauserUpdated(address indexed oldPartialPauser, address indexed newPartialPauser);
-    /// @notice emits an event when the system partial paused status changes
-    event SystemPartiallyPaused(bool isPaused);
-    /// @notice emits an event when the system fully paused status changes
-    event SystemFullyPaused(bool isPaused);
-    /// @notice emits an event when the call action restriction changes
-    event CallRestricted(bool isRestricted);
-    /// @notice emits an event when a donation transfer executed
-    event Donated(address indexed donator, address indexed asset, uint256 amount);
-
  
-    function donate(address _asset, uint256 _amount) external {}
+    function donate(address _asset, uint256 _amount) external;
 
    
-    function setOperator(address _operator, bool _isOperator) external {
-        require(operators[msg.sender][_operator] != _isOperator, "CO9");
-
-        operators[msg.sender][_operator] = _isOperator;
-
-        emit AccountOperatorUpdated(msg.sender, _operator, _isOperator);
-    }
+    function setOperator(address _operator, bool _isOperator) external ;
 
 
     function operate(Actions.ActionArgs[] memory _actions) external;
@@ -147,14 +45,14 @@ interface Controller {
             uint256
         );
 
-    function getPayout(address _otoken, uint256 _amount) public view returns (uint256);
+    function getPayout(address _otoken, uint256 _amount) external view returns (uint256);
     
     function isSettlementAllowed(
         address _underlying,
         address _strike,
         address _collateral,
         uint256 _expiry
-    ) public view returns (bool);
+    ) external view returns (bool);
 
     
     function getAccountVaultCounter(address _accountOwner) external view returns (uint256);
@@ -162,7 +60,7 @@ interface Controller {
     function hasExpired(address _otoken) external view returns (bool);
     
     function getVault(address _owner, uint256 _vaultId)
-        public
+        external
         view
         returns (
             MarginVault.Vault memory,
@@ -170,3 +68,4 @@ interface Controller {
             uint256
         );
 
+}
